14b868c56ad9cc501b73d4ecef7fc7882455c7d3
SONEKT <shulmanlevgen@gmail.com>
2023-09-01 17:49:02 +0500

    1

---
+++ src/main/java/com/example/KursovaiaDwa_PopitkaDwa/KursovaiaDwaPopitkaDwaApplication.java
@@ -0,0 +1,13 @@
+package com.example.KursovaiaDwa_PopitkaDwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursovaiaDwaPopitkaDwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursovaiaDwaPopitkaDwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KursovaiaDwa_PopitkaDwa/KursovaiaDwaPopitkaDwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KursovaiaDwa_PopitkaDwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovaiaDwaPopitkaDwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
